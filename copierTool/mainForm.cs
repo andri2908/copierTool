85b5cb203ca40d7954786e5e25caff0c1756350f
andri2908 <prasetya.andri@gmail.com>
2017-01-17 12:46:49 +0700

    Add Small Tool to do file copy to App Data Folder
    File Copy based on extension
    
    e.g. *.txt, *.xml, etc

---
+++ copierTool/mainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace copierTool
+{
+    public partial class mainForm : Form
+    {
+        private string[] fileExtList = { "*.lic", "*.xml" };
+        private string volumeLicensePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AlphaSoft\\";
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            string fileName = "";
+            if (!Directory.Exists(volumeLicensePath))
+                Directory.CreateDirectory(volumeLicensePath);
+
+            for (int i = 0; i < fileExtList.Count(); i++)
+            {
+                string[] fileNameList = Directory.GetFiles(Application.StartupPath, fileExtList[i]);
+
+                for (int j = 0; j < fileNameList.Count(); j++)
+                {
+                    fileName = fileNameList[j].Substring(fileNameList[j].LastIndexOf("\\") + 1);
+                    Console.WriteLine(fileName);
+
+                    File.Copy(fileName, volumeLicensePath + fileName, true);
+                }
+            }
+
+            this.Close();
+        }
+    }
+}
